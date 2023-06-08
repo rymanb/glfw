d657f1182d6eefc2b61a39f7d1e61b5957e5ea44
Ryman Barnett <boneyshark22@gmail.com>
2023-06-08 04:14:20 -0700

    my premake

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "off"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -76,6 +76,7 @@ project "GLFW"
 	filter "system:windows"
 		systemversion "latest"
 
+
 		files
 		{
 			"src/win32_init.c",
@@ -103,8 +104,3 @@ project "GLFW"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
-
-	filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
